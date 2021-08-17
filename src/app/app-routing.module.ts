4371c95d9dc607c4b4451a0cfcfe1b4a09730d88
娜一抹惊鸿 <18610649031@163.com>
2021-08-17 18:02:28 +0800

    feat: 完成前端概览

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,9 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', pathMatch: 'full', redirectTo: '/welcome' },
+  { path: '', pathMatch: 'full', redirectTo: '/overview' },
+  { path: 'overview', loadChildren: () => import('./pages/overview/overview.module').then(m => m.OverviewModule) },
   { path: 'welcome', loadChildren: () => import('./pages/welcome/welcome.module').then(m => m.WelcomeModule) },
-  { path: 'overview', loadChildren: () => import('./pages/overview/overview.module').then(m => m.OverviewModule) }
 ];
 
 @NgModule({
