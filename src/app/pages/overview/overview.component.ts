5d3801dea654fa782cdad1df079f6c9ffe3ae8e1
娜一抹惊鸿 <18610649031@163.com>
2021-12-08 10:31:58 +0800

    styles: 调整样式

--- src/app/pages/overview/overview.component.ts
+++ src/app/pages/overview/overview.component.ts
@@ -2,7 +2,22 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-overview',
-  template: '<markdown src="assets/markdown/overview.md"></markdown>'
+  template: '<markdown src="assets/markdown/overview.md"></markdown>',
+  styles: [
+    `
+      :host ::ng-deep {
+
+        table{
+          width: 100%;
+        }
+
+        td,th{
+          padding: 10px;
+          border: 1px solid #000;
+        }
+      }
+    `
+  ]
 })
 export class OverviewComponent implements OnInit {
 
